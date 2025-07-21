18e9e3beb067401941d701907007aa37d5f318fb
YasarDogann <yasardgn99@gmail.com>
2025-07-21 19:21:04 +0300

    Application Katmanı oluşturuldu

---
+++ RentCarServer/src/RentCarServer.Application/RegistrarService.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using RentCarServer.Application.Behaviors;
+using TS.MediatR;
+
+namespace RentCarServer.Application;
+public static class RegistrarService
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfr =>
+        {
+            cfr.RegisterServicesFromAssembly(typeof(RegistrarService).Assembly);
+            cfr.AddOpenBehavior(typeof(ValidationBehavior<,>)); // ressponce olduğu için "," koy response olmasa "," koyulmazdı
+            cfr.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(RegistrarService).Assembly); // Fluent Validation oto çalışması için eklemek lazım
+
+        return services;
+    }
+}
