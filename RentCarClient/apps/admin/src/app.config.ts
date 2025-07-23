706c3584cac3179a168b52d34d219cf8b1c7ceb5
YasarDogann <yasardgn99@gmail.com>
2025-07-23 17:48:40 +0300

    login page teması hazırlandı ve eklendi

--- RentCarClient/apps/admin/src/app.config.ts
+++ RentCarClient/apps/admin/src/app.config.ts
@@ -6,13 +6,14 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './interceptors/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(appRoutes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([httpInterceptor]))
   ],
 };
--- RentCarClient/apps/admin/src/app.routes.ts
+++ RentCarClient/apps/admin/src/app.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login')
+  },
+  {
     path: '',
     loadComponent: () => import('./pages/layouts/layouts'),
     children: [
