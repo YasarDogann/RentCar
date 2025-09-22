1db9df569cad3febf6390790aac95dfd7fbe8e04
YasarDogann <yasardgn99@gmail.com>
2025-09-22 13:47:50 +0300

    .NET'de Branch delete methodu refactor yapıldı ve Get methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Branches/BranchGetQuery.cs
@@ -0,0 +1,22 @@
+﻿using RentCarServer.Domain.Branches;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Branches;
+public sealed record BranchGetQuery(
+    Guid Id) : IRequest<Result<Branch>>;
+
+internal sealed class BranchGetQueryHandler(
+    IBranchRepository branchRepository) : IRequestHandler<BranchGetQuery, Result<Branch>>
+{
+    public async Task<Result<Branch>> Handle(BranchGetQuery request, CancellationToken cancellationToken)
+    {
+        var branch = await branchRepository.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+        if (branch is null)
+        {
+            return Result<Branch>.Failure("Şube bulunamadı");
+        }
+
+        return branch;
+    }
+}
