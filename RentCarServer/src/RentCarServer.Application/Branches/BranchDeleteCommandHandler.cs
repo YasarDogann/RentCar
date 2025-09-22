992f2675cbdeabb5e70e5d3a1ecf0b78b35bc741
YasarDogann <yasardgn99@gmail.com>
2025-09-22 13:33:27 +0300

    .NET'de Branch Delete methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Branches/BranchDeleteCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using GenericRepository;
+using RentCarServer.Domain.Branches;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Branches;
+public sealed record BranchDeleteCommand(
+    Guid Id) : IRequest<Result<string>>;
+
+internal sealed class BranchDeleteCommandHandler(
+    IBranchRepository branchRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<BranchDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(BranchDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var branch = await branchRepository.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+        if (branch is null)
+        {
+            return Result<string>.Failure("Şube bulunamadı");
+        }
+
+        branch.Delete();
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Şube başarıyla silindi";
+    }
+}
