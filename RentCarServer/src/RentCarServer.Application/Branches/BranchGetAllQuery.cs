2c64dcaa1e9574e04e154894ba42811bb226ad5c
YasarDogann <yasardgn99@gmail.com>
2025-09-23 10:11:17 +0300

    .NET'de Branch GetAll methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Branches/BranchGetAllQuery.cs
@@ -0,0 +1,49 @@
+﻿using RentCarServer.Domain.Abstractions;
+using RentCarServer.Domain.Branches;
+using RentCarServer.Domain.Branches.ValueObjects;
+using RentCarServer.Domain.Users;
+using TS.MediatR;
+
+namespace RentCarServer.Application.Branches;
+public sealed record BranchGetAllQuery : IRequest<IQueryable<BranchGetAllQueryResponse>>;
+
+public sealed class BranchGetAllQueryResponse : EntityDto
+{
+    public string Name { get; set; } = default!;
+    public Address Address { get; set; } = default!;
+}
+
+internal sealed class BranchGetAllQueryHandler(
+    IBranchRepository branchRepository,
+    IUserRepository userRepository) : IRequestHandler<BranchGetAllQuery, IQueryable<BranchGetAllQueryResponse>>
+{
+    public Task<IQueryable<BranchGetAllQueryResponse>> Handle(BranchGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var response = branchRepository
+            .GetAll()
+            .Join(userRepository.GetAll(), m => m.CreatedBy, m => m.Id, (b, user) => new { b = b, user = user })
+            .GroupJoin(userRepository.GetAll(), m => m.b.UpdatedBy, m => m.Id, (entity, user) => new { entity = entity, user = user })
+            .SelectMany(s => s.user.DefaultIfEmpty(),
+                (x, user) => new
+                {
+                    entity = x.entity,
+                    updatedUser = x.user
+                })
+            .Select(s => new BranchGetAllQueryResponse
+            {
+                Id = s.entity.b.Id,
+                Name = s.entity.b.Name.Value,
+                Address = s.entity.b.Address,
+                CreatedAt = s.entity.b.CreatedAt,
+                CreatedBy = s.entity.b.CreatedBy,
+                IsActive = s.entity.b.IsActive,
+                UpdatedAt = s.entity.b.UpdatedAt,
+                UpdatedBy = s.entity.b.UpdatedBy,
+                CreatedFullName = s.entity.user.FullName.Value,
+                UpdatedFullName = !s.updatedUser.Any() ? null : s.updatedUser.First().FullName.Value
+            })
+            .AsQueryable();
+
+        return Task.FromResult(response);
+    }
+}
---
+++ RentCarServer/src/RentCarServer.Domain/Abstractions/EntityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RentCarServer.Domain.Abstractions;
+public abstract class EntityDto
+{
+    public IdentityId Id { get; set; } = default!;
+    public bool IsActive { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public IdentityId CreatedBy { get; set; } = default!;
+    public string CreatedFullName { get; set; } = default!;
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public IdentityId? UpdatedBy { get; set; }
+    public string? UpdatedFullName { get; set; }
+}
