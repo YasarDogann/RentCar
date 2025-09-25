f676a4ce9ec3d1ace03bc21b95c2cba76c382b1f
YasarDogann <yasardgn99@gmail.com>
2025-09-25 12:02:04 +0300

    .NET'de Role Get methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Roles/RoleDto.cs
@@ -0,0 +1,29 @@
+﻿using RentCarServer.Domain.Abstractions;
+using RentCarServer.Domain.Roles;
+
+namespace RentCarServer.Application.Roles;
+public sealed class RoleDto : EntityDto
+{
+    public string Name { get; set; } = default!;
+}
+
+public static class RoleExtensions
+{
+    public static IQueryable<RoleDto> MapTo(this IQueryable<EntityWithAuditDto<Role>> entites)
+    {
+        var res = entites.Select(s => new RoleDto
+        {
+            Id = s.Entity.Id,
+            Name = s.Entity.Name.Value,
+            IsActive = s.Entity.IsActive,
+            CreatedAt = s.Entity.CreatedAt,
+            CreatedBy = s.Entity.CreatedBy,
+            CreatedFullName = s.CreatedUser.FullName.Value,
+            UpdatedAt = s.Entity.UpdatedAt,
+            UpdatedBy = s.Entity.UpdatedBy != null ? s.Entity.UpdatedBy.Value : null,
+            UpdatedFullName = s.UpdatedUser != null ? s.UpdatedUser.FullName.Value : null,
+        }).AsQueryable();
+
+        return res;
+    }
+}
---
+++ RentCarServer/src/RentCarServer.Application/Roles/RoleGetQuery.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarServer.Domain.Roles;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Roles;
+public sealed record RoleGetQuery(
+    Guid Id) : IRequest<Result<RoleDto>>;
+
+internal sealed class RoleGetQueryHandler(
+    IRoleRepository roleRepository) : IRequestHandler<RoleGetQuery, Result<RoleDto>>
+{
+    public async Task<Result<RoleDto>> Handle(RoleGetQuery request, CancellationToken cancellationToken)
+    {
+        var res = await roleRepository
+            .GetAllWithAudit()
+            .MapTo()
+            .Where(p => p.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (res is null)
+        {
+            return Result<RoleDto>.Failure("Rol bulunamadı");
+        }
+
+        return res;
+    }
+}
