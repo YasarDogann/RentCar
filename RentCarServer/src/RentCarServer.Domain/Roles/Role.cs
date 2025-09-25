27511c6a4600464652ea845a64fb23aff9f17d98
YasarDogann <yasardgn99@gmail.com>
2025-09-25 11:17:26 +0300

    .NET'de Role Create methodu oluşturuldu

--- RentCarServer/src/RentCarServer.Domain/Roles/Role.cs
+++ RentCarServer/src/RentCarServer.Domain/Roles/Role.cs
@@ -4,5 +4,17 @@ using RentCarServer.Domain.Shared;
 namespace RentCarServer.Domain.Roles;
 public sealed class Role : Entity
 {
+    private Role() { }
+    public Role(Name name)
+    {
+        SetName(name);
+    }
     public Name Name { get; private set; } = default!;
+
+    #region Behaviors
+    public void SetName(Name name)
+    {
+        Name = name;
+    }
+    #endregion
 }
