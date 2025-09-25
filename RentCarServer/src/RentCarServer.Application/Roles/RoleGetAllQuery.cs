afacc10f9624d99ad16f71a9facaf8e0da0519e4
YasarDogann <yasardgn99@gmail.com>
2025-09-25 12:57:40 +0300

    .NET'de Role GetAll methodu eklendi ve refactor yapıldı.

---
+++ RentCarServer/src/RentCarServer.Application/Roles/RoleGetAllQuery.cs
@@ -0,0 +1,12 @@
+﻿using RentCarServer.Domain.Roles;
+using TS.MediatR;
+
+namespace RentCarServer.Application.Roles;
+public sealed record RoleGetAllQuery : IRequest<IQueryable<RoleDto>>;
+
+internal sealed class RoleGetAllQueryHandler(
+    IRoleRepository roleRepository) : IRequestHandler<RoleGetAllQuery, IQueryable<RoleDto>>
+{
+    public Task<IQueryable<RoleDto>> Handle(RoleGetAllQuery request, CancellationToken cancellationToken) =>
+        Task.FromResult(roleRepository.GetAllWithAudit().MapTo().AsQueryable());
+}
