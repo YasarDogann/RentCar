c4096c6dcd383e01b4be66aabb7b5c07d6512d98
YasarDogann <yasardgn99@gmail.com>
2025-09-25 11:34:29 +0300

    .NET'de Role Delete methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Roles/RoleDeleteCommand.cs
@@ -0,0 +1,28 @@
+﻿using GenericRepository;
+using RentCarServer.Domain.Roles;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Roles;
+public sealed record RoleDeleteCommand(
+    Guid Id) : IRequest<Result<string>>;
+
+internal sealed class RoleDeleteCommandHandler(
+    IRoleRepository roleRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<RoleDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RoleDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var role = await roleRepository.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+        if (role is null)
+        {
+            return Result<string>.Failure("Rol bulunamadı");
+        }
+
+        role.Delete();
+        roleRepository.Update(role);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Rol başarıyla silindi";
+    }
+}
