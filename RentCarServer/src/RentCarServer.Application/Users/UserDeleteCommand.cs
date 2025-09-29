08f2a5b6abaa6adae3b2560b7765dc9c5fa75bef
YasarDogann <yasardgn99@gmail.com>
2025-09-29 17:59:30 +0300

    .NET'de User Create ve update methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Users/UserDeleteCommand.cs
@@ -0,0 +1,28 @@
+﻿using GenericRepository;
+using RentCarServer.Domain.Users;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Users;
+public sealed record UserDeleteCommand(
+    Guid Id) : IRequest<Result<string>>;
+
+internal sealed class UserDeleteCommandHandler(
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<UserDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UserDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+        if (user is null)
+        {
+            return Result<string>.Failure("Kullanıcı bulunamadı");
+        }
+
+        user.Delete();
+        userRepository.Update(user);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Kullanıcı başarıyla silindi";
+    }
+}
