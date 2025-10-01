7e921011dda5075a021d4aec3adfbdc614d5b764
YasarDogann <yasardgn99@gmail.com>
2025-10-01 19:43:40 +0300

    .NET'de Category GetAll methodu ai ile eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Categories/CategoryGetAllQuery.cs
@@ -0,0 +1,13 @@
+﻿using RentCarServer.Domain.Categories;
+using TS.MediatR;
+
+namespace RentCarServer.Application.Categories;
+
+public sealed record CategoryGetAllQuery : IRequest<IQueryable<CategoryDto>>;
+
+internal sealed class CategoryGetAllQueryHandler(
+    ICategoryRepository categoryRepository) : IRequestHandler<CategoryGetAllQuery, IQueryable<CategoryDto>>
+{
+    public Task<IQueryable<CategoryDto>> Handle(CategoryGetAllQuery request, CancellationToken cancellationToken) =>
+        Task.FromResult(categoryRepository.GetAllWithAudit().MapToGet().AsQueryable());
+}
