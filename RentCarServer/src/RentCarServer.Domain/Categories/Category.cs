5d88691e6059bdcb724d210b4f7534c0e0b1b1fe
YasarDogann <yasardgn99@gmail.com>
2025-10-01 19:14:45 +0300

    .NET'de Category Model oluşturuldu

---
+++ RentCarServer/src/RentCarServer.Domain/Categories/Category.cs
@@ -0,0 +1,21 @@
+﻿using RentCarServer.Domain.Abstractions;
+using RentCarServer.Domain.Shared;
+
+namespace RentCarServer.Domain.Categories;
+public sealed class Category : Entity
+{
+    private Category() { }  
+    
+    public Category(Name name, bool isActive)
+    {
+        SetName(name);
+        SetStatus(isActive);
+    }
+
+    public Name Name { get; private set; } = default!;
+
+    public void SetName(Name name)
+    {
+        Name = name;
+    }
+}
