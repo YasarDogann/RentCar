d633d52c85d2e8cb77db4e68373bc78822e281a5
YasarDogann <yasardgn99@gmail.com>
2025-10-01 19:29:26 +0300

    .NET'de Category Create Methodu oluşturuldu

---
+++ RentCarServer/src/RentCarServer.Application/Categories/CategoryCreateCommand.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using GenericRepository;
+using RentCarServer.Domain.Categories;
+using RentCarServer.Domain.Shared;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Categories;
+
+public sealed record CategoryCreateCommand(
+    string Name,
+    bool IsActive) : IRequest<Result<string>>;
+
+public sealed class CategoryCreateCommandValidator : AbstractValidator<CategoryCreateCommand>
+{
+    public CategoryCreateCommandValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().WithMessage("Geçerli bir kategori adı girin");
+    }
+}
+
+internal sealed class CategoryCreateCommandHandler(
+    ICategoryRepository categoryRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<CategoryCreateCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(CategoryCreateCommand request, CancellationToken cancellationToken)
+    {
+        var nameExists = await categoryRepository.AnyAsync(p => p.Name.Value == request.Name, cancellationToken);
+
+        if (nameExists)
+        {
+            return Result<string>.Failure("Kategori adı daha önce tanımlanmış");
+        }
+
+        Name name = new(request.Name);
+        Category category = new(name, request.IsActive);
+        categoryRepository.Add(category);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Kategori başarıyla kaydedildi";
+    }
+}
