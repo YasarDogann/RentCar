904844ded85f868b730e17ce493df020a7ef322f
YasarDogann <yasardgn99@gmail.com>
2025-10-01 19:41:28 +0300

    .NET'de category get methodu eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Categories/CategoryDto.cs
@@ -0,0 +1,28 @@
+﻿using RentCarServer.Domain.Abstractions;
+using RentCarServer.Domain.Categories;
+
+namespace RentCarServer.Application.Categories;
+
+public sealed class CategoryDto : EntityDto
+{
+    public string Name { get; set; } = default!;
+}
+
+public static class CategoryExtensions
+{
+    public static IQueryable<CategoryDto> MapToGet(this IQueryable<EntityWithAuditDto<Category>> entities)
+    {
+        return entities.Select(s => new CategoryDto
+        {
+            Id = s.Entity.Id,
+            Name = s.Entity.Name.Value,
+            IsActive = s.Entity.IsActive,
+            CreatedAt = s.Entity.CreatedAt,
+            CreatedBy = s.Entity.CreatedBy,
+            CreatedFullName = s.CreatedUser.FullName.Value,
+            UpdatedAt = s.Entity.UpdatedAt,
+            UpdatedBy = s.Entity.UpdatedBy != null ? s.Entity.UpdatedBy.Value : null,
+            UpdatedFullName = s.UpdatedUser != null ? s.UpdatedUser.FullName.Value : null,
+        });
+    }
+}
---
+++ RentCarServer/src/RentCarServer.Application/Categories/CategoryGetQuery.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarServer.Domain.Categories;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Categories;
+
+public sealed record CategoryGetQuery(
+    Guid Id) : IRequest<Result<CategoryDto>>;
+
+internal sealed class CategoryGetQueryHandler(
+    ICategoryRepository categoryRepository) : IRequestHandler<CategoryGetQuery, Result<CategoryDto>>
+{
+    public async Task<Result<CategoryDto>> Handle(CategoryGetQuery request, CancellationToken cancellationToken)
+    {
+        var res = await categoryRepository
+            .GetAllWithAudit()
+            .MapToGet()
+            .Where(p => p.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (res is null)
+        {
+            return Result<CategoryDto>.Failure("Kategori bulunamadı");
+        }
+
+        return res;
+    }
+}
