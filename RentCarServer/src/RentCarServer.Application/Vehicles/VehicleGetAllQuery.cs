d8d088648d63c55999e635e57835cc549350ee56
YasarDogann <yasardgn99@gmail.com>
2025-10-13 13:02:30 +0300

    .NET'de Vehicle Get ve GetAll methodları eklendi. Update methodunda refactor yapıldı. (Copilot AI)

---
+++ RentCarServer/src/RentCarServer.Application/Vehicles/VehicleGetAllQuery.cs
@@ -0,0 +1,15 @@
+﻿using RentCarServer.Application.Behaviors;
+using RentCarServer.Domain.Vehicles;
+using TS.MediatR;
+
+namespace RentCarServer.Application.Vehicles;
+
+[Permission("vehicle:view")]
+public sealed record VehicleGetAllQuery : IRequest<IQueryable<VehicleDto>>;
+
+internal sealed class VehicleGetAllQueryHandler(
+    IVehicleRepository vehicleRepository) : IRequestHandler<VehicleGetAllQuery, IQueryable<VehicleDto>>
+{
+    public Task<IQueryable<VehicleDto>> Handle(VehicleGetAllQuery request, CancellationToken cancellationToken) =>
+        Task.FromResult(vehicleRepository.GetAllWithAudit().MapTo().AsQueryable());
+}
---
+++ RentCarServer/src/RentCarServer.Application/Vehicles/VehicleGetQuery.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarServer.Application.Behaviors;
+using RentCarServer.Domain.Vehicles;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Vehicles;
+
+[Permission("vehicle:view")]
+public sealed record VehicleGetQuery(Guid Id) : IRequest<Result<VehicleDto>>;
+
+internal sealed class VehicleGetQueryHandler(
+    IVehicleRepository vehicleRepository) : IRequestHandler<VehicleGetQuery, Result<VehicleDto>>
+{
+    public async Task<Result<VehicleDto>> Handle(VehicleGetQuery request, CancellationToken cancellationToken)
+    {
+        var res = await vehicleRepository
+            .GetAllWithAudit()
+            .MapTo()
+            .Where(p => p.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (res is null)
+            return Result<VehicleDto>.Failure("Araç bulunamadı");
+
+        return res;
+    }
+}
