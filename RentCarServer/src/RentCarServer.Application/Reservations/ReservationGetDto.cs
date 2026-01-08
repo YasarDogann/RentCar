c1f20fa23425f28cdf8d4f6ef7686392d74b066b
YasarDogann <yasardgn99@gmail.com>
2026-01-08 22:33:59 +0300

    Reservation : GetDto'da refactor yapıldı

--- RentCarServer/src/RentCarServer.Application/Reservations/ReservationGetDto.cs
+++ RentCarServer/src/RentCarServer.Application/Reservations/ReservationGetDto.cs
@@ -1,36 +1,34 @@
 ﻿using RentCarServer.Domain.Abstractions;
+using RentCarServer.Domain.Reservations.ValueObjects;
 
 namespace RentCarServer.Application.Reservations;
-public sealed class ReservationGetDto : EntityDto
+public sealed class PickUpDto
 {
-    public Guid CustomerId { get; set; } = default!;
-    public string CustomerFullName { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string FullAddress { get; set; } = default!;
+    public string PhoneNumber { get; set; } = default!;
+}
+
+public sealed class CustomerDto
+{
+    public string FullName { get; set; } = default!;
     public string IdentityNumber { get; set; } = default!;
     public string PhoneNumber { get; set; } = default!;
     public string Email { get; set; } = default!;
     public string FullAddress { get; set; } = default!;
-    public DateTime ReservationDate { get; set; } = default!;
-    public DateTime DeliveryDate { get; set; } = default!;
-    public string Status { get; set; } = default!;
-    public int TotalDay { get; set; } = default!;
-    public Guid VehicleId { get; set; } = default!;
-    public string VehicleName { get; set; } = default!;
-    public string VehicleBrand { get; set; } = default!;
-    public string VehicleModel { get; set; } = default!;
-    public int VehicleModelYear { get; set; } = default!;
-    public string VehicleColor { get; set; } = default!;
-    public string VehicleCategoryName { get; set; } = default!;
+}
+public sealed class VehicleDto
+{
+    public string Name { get; set; } = default!;
+    public string Brand { get; set; } = default!;
+    public string Model { get; set; } = default!;
+    public int ModelYear { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public string CategoryName { get; set; } = default!;
     public string FuelConsumption { get; set; } = default!;
     public int SeatCount { get; set; } = default!;
     public string TractionType { get; set; } = default!;
-    public Guid PickupLocationId { get; set; } = default!;
-    public string PickupLocationName { get; set; } = default!;
-    public string PickupLocationFullAddress { get; set; } = default!;
-    public string PickupLocationPhoneNumber { get; set; } = default!;
-    public Guid ProtectionPackageId { get; set; } = default!;
-    public string ProtectionPackageName { get; set; } = default!;
-    public decimal ProtectionPackagePrice { get; set; } = default!;
-    public List<ReservationExtraDto> ReservationExtras { get; set; } = default!;
+
 }
 
 
@@ -39,4 +37,27 @@ public sealed class ReservationExtraDto
     public Guid ExtraId { get; set; }
     public string ExtraName { get; set; } = default!;
     public decimal Price { get; set; }
+}
+public sealed class ReservationGetDto : EntityDto
+{
+    public Guid CustomerId { get; set; } = default!;
+    public CustomerDto Customer { get; set; } = default!;
+    public Guid PickUpLocationId { get; set; } = default!;
+    public PickUpDto PickUp { get; set; } = default!;
+    public DateOnly PickUpDate { get; set; } = default!;
+    public TimeOnly PickUpTime { get; set; } = default!;
+    public DateOnly DeliveryDate { get; set; } = default!;
+    public DeliveryTime DeliveryTime { get; set; } = default!;
+    public Guid VehicleId { get; set; } = default!;
+    public decimal VehicleDailyPrice { get; set; } = default!;
+    public VehicleDto Vehicle { get; set; } = default!;
+    public Guid ProtectionPackageId { get; set; } = default!;
+    public decimal ProtectionPackagePrice { get; set; } = default!;
+    public string ProtectionPackageName { get; set; } = default!;
+
+    public List<ReservationExtraDto> ReservationExtras { get; set; } = default!;
+    public string Note { get; set; } = default!;
+    public decimal Total { get; set; } = default!;
+    public string Status { get; set; } = default!;
+    public int TotalDay { get; set; } = default!;
 }
