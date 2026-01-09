ccf06aec0c5f69f57a03a50512a615ea699ab019
YasarDogann <yasardgn99@gmail.com>
2026-01-09 21:32:16 +0300

    Reservation MapTo, Get ve GetAll metotları eklendi

---
+++ RentCarServer/src/RentCarServer.Application/Reservations/ReservationGetAllQuery.cs
@@ -0,0 +1,34 @@
+﻿using RentCarServer.Domain.Branches;
+using RentCarServer.Domain.Categories;
+using RentCarServer.Domain.Customers;
+using RentCarServer.Domain.Extras;
+using RentCarServer.Domain.ProtectionPackages;
+using RentCarServer.Domain.Reservations;
+using RentCarServer.Domain.Vehicles;
+using TS.MediatR;
+
+namespace RentCarServer.Application.Reservations;
+public sealed record ReservationGetAllQuery : IRequest<IQueryable<ReservationDto>>;
+
+internal sealed class ReservationGetAllQueryHandler(
+    IReservationRepository reservationRepository,
+    IQueryable<Customer> customers,
+    IQueryable<Branch> branches,
+    IQueryable<Vehicle> vehicles,
+    IQueryable<Category> categories,
+    IQueryable<ProtectionPackage> protectionPackages,
+    IQueryable<Extra> extras
+    ) : IRequestHandler<ReservationGetAllQuery, IQueryable<ReservationDto>>
+{
+    public Task<IQueryable<ReservationDto>> Handle(ReservationGetAllQuery request, CancellationToken cancellationToken) =>
+        Task.FromResult(
+            reservationRepository.GetAllWithAudit()
+            .MapTo(
+                customers,
+                branches,
+                vehicles,
+                categories,
+                protectionPackages,
+                extras)
+            .AsQueryable());
+}
---
+++ RentCarServer/src/RentCarServer.Application/Reservations/ReservationGetQuery.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarServer.Domain.Branches;
+using RentCarServer.Domain.Categories;
+using RentCarServer.Domain.Customers;
+using RentCarServer.Domain.Extras;
+using RentCarServer.Domain.ProtectionPackages;
+using RentCarServer.Domain.Reservations;
+using RentCarServer.Domain.Vehicles;
+using TS.MediatR;
+using TS.Result;
+
+namespace RentCarServer.Application.Reservations;
+public sealed record ReservationGetQuery(
+    Guid Id) : IRequest<Result<ReservationDto>>;
+
+internal sealed class ReservationGetQueryHandler(
+    IReservationRepository reservationRepository,
+    IQueryable<Customer> customers,
+    IQueryable<Branch> branches,
+    IQueryable<Vehicle> vehicles,
+    IQueryable<Category> categories,
+    IQueryable<ProtectionPackage> protectionPackages,
+    IQueryable<Extra> extras
+    ) : IRequestHandler<ReservationGetQuery, Result<ReservationDto>>
+{
+    public async Task<Result<ReservationDto>> Handle(ReservationGetQuery request, CancellationToken cancellationToken)
+    {
+        var res = await reservationRepository.GetAllWithAudit().MapTo(
+            customers,
+            branches,
+            vehicles,
+            categories,
+            protectionPackages,
+            extras)
+            .Where(i => i.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (res is null)
+        {
+            return Result<ReservationDto>.Failure("Rezervasyon bulunamadı");
+        }
+
+        return res;
+    }
+}
